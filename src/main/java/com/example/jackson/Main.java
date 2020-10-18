da88e7fcee4aca3cb59e78a1dc7f282817aa5bf0
Chivukula Sree Mrudula <chivukula.mrudula@gmail.com>
2020-10-18 23:25:16 +0530

    Initial Commit

--- src/main/java/com/example/jackson/Main.java
+++ src/main/java/com/example/jackson/Main.java
@@ -4,16 +4,70 @@ import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.lang.model.util.ElementScanner6;
+
 class Main {
 
   // Optional
-  public void parseJsonManually(File file) {
+  public void parseJsonManually(File file) throws FileNotFoundException, IOException {
+    if(true) {
+      ObjectMapper objectMapper = new ObjectMapper();
+      Trade[] trades = objectMapper.readValue(file, Trade[].class);
+      for (Trade trade : trades) {
+        System.out.println(trade);
+      }
+    }
+    else 
+      manualParser(file);
 
+  }
+  private void manualParser(File file) throws FileNotFoundException, IOException {
+    BufferedReader br = new BufferedReader(new FileReader(file));
+    String str,symbol="", purchaseDate="";
+    int qty=0;
+    Trade tr;
+    while((str = br.readLine())!=null) {
+      if(str.contains("[") || str.contains("{") || str.contains("]"))
+        continue;
+      else if(str.contains("}")){
+        tr = new Trade(symbol, qty, purchaseDate);
+        System.out.println(tr);
+      }
+      else{
+        int first = str.indexOf(":");
+        int last;
+        if(str.charAt(str.length()-1)=='"')
+          last = str.length()-1;
+        else if(str.charAt(str.length()-1)!='"' && str.charAt(str.length()-1)!=',')
+          last = str.length();
+        else if (str.charAt(str.length()-2)=='"')
+          last = str.length()-2;
+        else
+          last = str.length()-1;
+         if(str.contains("symbol")){
+          first+=3;
+           symbol = str.substring(first,last);
+         }
+         else if(str.contains("purchaseDate")){
+          first+=3;
+           purchaseDate = str.substring(first, last);
+         }
+         else if(str.contains("quantity")){
+          first+=2;
+           qty = Integer.parseInt(str.substring(first,last));
+         }
+         
+      }
+    }
+    br.close();
   }
 
   public void parseJsonJacksomatically(File inputFile, File outputFile) throws JsonParseException, JsonMappingException, IOException {
@@ -47,7 +101,7 @@ class Main {
     }
 
     // TODO: Add method arguments
-    // objectMapper.writeValue(, );
+    objectMapper.writeValue(outputFile, impTrades);
   }
 
   // Milestone 4
@@ -65,7 +119,7 @@ class Main {
     objectMapper.writeValue(outputFile, trades);
     System.out.println("Serialized(wrote) successfully");
   }
-
+  
   public static void main(String[] args) throws JsonParseException, JsonMappingException, IOException {
     Main main = new Main();
 
@@ -79,17 +133,17 @@ class Main {
     File outputFile = new File("src/main/java/com/example/jackson/readOutput.json");
 
     // uncomment below line to run your manual json parsing logic
-    // main.parseJsonManually(trades);
+    main.parseJsonManually(trades);
 
     /* 
     /* uncomment below line to use Jackson
     /* use json input files as said in instructions
     /* Files - trades/tradesFancy/tradesFancier
     */
-    // main.parseJsonJacksomatically(tradesFancier, outputFile);
+    //main.parseJsonJacksomatically(tradesFancier, outputFile);
 
     // uncomment this to 
-    // main.writeImportantPurchasesToFile(trades, impTrades);
+    main.writeImportantPurchasesToFile(50,trades, impTrades);
 
     // Uncomment in Milestone 4
     // main.parseJsonJacksomaticallyPrivate(tradesFancier, outputFile);
